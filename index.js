843404573c67e0a910dfe55c407d3cb575628fe1
DonHikmato <hikmaterino.rahman011228@gmail.com>
2022-11-07 14:07:05 +0100

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+async function getWeather() {
+  const input = document.getElementById("search-bar");
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${
+      input.value || "Skövde"
+    }&appid=fc4b84c9e65b1dfc8368ad0c040e4539&units=metric`
+  );
+  const data = await res.json();
+  console.log(data);
+
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
+  const speedMeterPerSec = speed / 3.4;
+
+  document.querySelector(".city").innerText = "Weather in " + name;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+  document.querySelector(".weather-description").innerText = description;
+  document.querySelector(".temp").innerText = Math.round(temp) + "°C";
+  document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+  document.querySelector(".wind").innerText =
+    "Wind speed: " + Math.round(speedMeterPerSec * 10) / 10 + " m/s";
+}
+
+document.querySelector(".search-button").addEventListener("click", () => {
+  getWeather();
+});
+
+document.getElementById("search-bar").addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    getWeather();
+  }
+});
+
+getWeather();
